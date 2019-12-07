29e851dbd4af45507ebac2e2a66dc56b5c6ba351
vicent1997 <rosanatividadvicent@gmail.com>
2019-12-07 16:58:36 +0100

    commit 1

--- src/app/login-registro/login/login.component.ts
+++ src/app/login-registro/login/login.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.sass"]
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
   public formGroup: FormGroup;
